fb41a25703baa2f15a5c9a9a0d57171ed3a37812
Daniel Rojo <destrada-2018259@kinal.edu.gt>
2023-04-17 21:40:07 -0600

    primer commit

---
+++ models/server.js
@@ -0,0 +1,45 @@
+const express = require('express');
+const cors = require('cors');
+const {dbConection} = require('../database/config');
+
+class Server{
+    constructor(){
+        //variables de configuración
+        this.app = express();
+        this.port = process.env.PORT;
+        //Conectar a la base de datos
+        this.conectarDB();
+
+        //middleware
+        this.middlewares();
+
+        //rutas de mi app
+
+        this.routes();
+    
+    }
+
+    //Metodo de conexion a mongo
+    async conectarDB(){
+        await dbConection();
+    }
+
+    middlewares(){
+
+        //cors
+        this.app.use(cors());
+        this.app.use(express.static('public'));
+         //Lectura y parseo del body
+         this.app.use( express.json() );
+    }
+
+    routes(){
+    }
+
+    listen(){
+        this.app.listen(this.port, () => {
+            console.log(`Servidor corriendo en puerto ${this.port}`);
+        })
+    }
+}
+module.exports = Server;
